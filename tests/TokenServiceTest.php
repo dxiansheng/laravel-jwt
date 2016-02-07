62ce00384b431b81239f00b94e4c7304e9a75623
Pascal Baljet <pascal@pascalbaljet.nl>
2016-02-07 23:17:58 +0100

    More testing

--- tests/TokenServiceTest.php
+++ tests/TokenServiceTest.php
@@ -4,6 +4,7 @@ namespace Pbmedia\Jwt\Test;
 
 use Illuminate\Support\Facades\Config;
 use Lcobucci\JWT\Token;
+use Mockery;
 use Orchestra\Testbench\TestCase;
 use Pbmedia\Jwt\TokenService;
 
@@ -57,11 +58,31 @@ class TokenServiceTest extends TestCase
 
     public function testFindUserByTokenOrFail()
     {
-        $token = (string) $this->getService()->generateTokenForUser(new UserStub);
+        $user = Mockery::mock(UserStub::class);
+
+        $user->shouldReceive('getQualifiedKeyForToken')
+            ->andReturn(1);
+
+        $token = (string) $this->getService()->generateTokenForUser($user);
+
+        Config::shouldReceive('get')
+            ->twice()
+            ->with('laravel-jwt.issuer')
+            ->andReturn('Pascal Baljet Media');
+
+        Config::shouldReceive('get')
+            ->once()
+            ->with('laravel-jwt.model')
+            ->andReturn(UserStub::class);
 
         $user = $this->getService()->findUserByTokenOrFail($token);
 
         $this->assertInstanceOf(UserStub::class, $user);
+    }
+
+    public function testTokenIsValid()
+    {
+        $token = (string) $this->getService()->generateTokenForUser(new UserStub);
         $this->assertTrue($this->getService()->tokenIsValid($token));
     }
 
